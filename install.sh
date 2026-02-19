448007d9025a3d824683fe2a806c25dc72770cc4
Sebastion <sebastionprice@gmail.com>
2026-02-19 16:21:26 +1100

    misc prompt adjustments and file save fixes for .bash's

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ gitsetup()
 		git config --global user.name "$3";
 		git config --global credential.helper 'cache --timeout=7200';
 
-		echo "and downloading your repops";
+		echo "and downloading your repos";
 		for r in $4; do
 			if [ ! -d "$HOME/$r" ]; then
 				git clone "https://github.com/$1/$r.git";
@@ -20,9 +20,9 @@ rmdf()
 		echo "${DF[*]}";
 		read -p $'y/n: ' -r remove;
 		if [[ "$remove" == "y" ]];then
+			echo "removing defualts!";
 			for d in $DF; do
-				echo "removing defualts!";
-				rm "$d";
+				rm -r "$d";
 			done		
 		else
 			echo "righty-o, not removing";
@@ -34,9 +34,9 @@ if [[ $SHELL == *bash ]]; then
 	read -p "continue: y/n " ans;
 	if [[ "$ans" == "y" ]]; then
 		echo "Hello and thank you for picking me :)";
-		read -p "Please enter your username " UN;
-		read -p "Please enter your name " NAME;
-		read -p "Please enter your email " EMAIL;
+		read -p "Please enter your username :" UN;
+		read -p "Please enter your name :" NAME;
+		read -p "Please enter your email :" EMAIL;
 		REPOS=("git-intro" "holbertonschool-shell" "holbertonschool-low_level_programming");
 
 		rmdf;
@@ -45,13 +45,13 @@ if [[ $SHELL == *bash ]]; then
 		if [[ -f ~/.bashrc ]]; then
 			mv $HOME/.bashrc $HOME/.bashrc.bak;
 		fi
-		wget https://raw.githubusercontent.com/Opabinia9/holberton-sandbox-setup/refs/heads/main/bashrc;
+		wget -qO ~/.bashrc https://raw.githubusercontent.com/Opabinia9/holberton-sandbox-setup/refs/heads/main/bashrc;
 
 		echo "getting aliases and saving your old ones";
 		if [[ -f $HOME/.bash_aliases ]]; then
 			mv $HOME/.bash_aliases $HOME/.bash_aliases.bak;
 		fi
-		wget https://raw.githubusercontent.com/Opabinia9/holberton-sandbox-setup/refs/heads/main/bash_aliases;
+		wget -qO ~/.bash_aliases https://raw.githubusercontent.com/Opabinia9/holberton-sandbox-setup/refs/heads/main/bash_aliases;
 
 		echo "configuring git";
 		gitsetup "$UN" "$EMAIL" "$NAME" "$REPOS";
